4598fc59a1b04b703136c490feaee21c99f9f30e
aled <aled@wibblr.com>
2022-12-02 11:05:09 +0000

    Require SFTP storage to specify a base directory for encrypted files

--- Wibblr.Grufs/SftpStorage.cs
+++ Wibblr.Grufs/SftpStorage.cs
@@ -12,38 +12,43 @@ namespace Wibblr.Grufs
 {
     public class SftpStorage : IFileStorage
     {
-        private string _host, _username, _password;
+        private string _host, _username, _password, _baseDir;
 
-        public SftpStorage(string host, string username, string password)
+        public SftpStorage(string host, string username, string password, string baseDir)
         {
             _host = host;
             _username = username;
             _password = password;
+            _baseDir = baseDir;
         }
 
         public byte[] Download(string path)
         {
+            var fullPath = Path.Combine(_baseDir, path).Replace("\\", "/"); ;
+
             using (var client = new SftpClient(_host, _username, _password))
             {
                 client.Connect();
-                return client.ReadAllBytes(path);
+                return client.ReadAllBytes(fullPath);
             }
         }
 
         public void Upload(string path, byte[] content)
         {
+            var fullPath = Path.Combine(_baseDir, path).Replace("\\", "/");
+
             using (var client = new SftpClient(_host, _username, _password))
             {
                 client.Connect();
 
-                if (!client.Exists(path))
+                if (!client.Exists(fullPath))
                 {
-                    foreach (var directory in ((IFileStorage)this).GetParentDirectories(path))
+                    foreach (var directory in ((IFileStorage)this).GetParentDirectories(fullPath))
                     {
                         if (!client.Exists(directory))
                             client.CreateDirectory(directory);
                     }
-                    using (var stream = client.OpenWrite(path))
+                    using (var stream = client.OpenWrite(fullPath))
                     {
                         stream.Write(content, 0, content.Length);
                     }
