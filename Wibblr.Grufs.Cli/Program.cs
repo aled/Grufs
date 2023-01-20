a9740f56fd45082210fbe301712e687305f6c73e
aled <aled@wibblr.com>
2023-01-20 00:29:39 +0000

    Add usage comment

--- Wibblr.Grufs.Cli/Program.cs
+++ Wibblr.Grufs.Cli/Program.cs
@@ -1,10 +1,52 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using System.Text.Json;
+﻿using System.Text.Json;
 
 using Wibblr.Grufs.Encryption;
 
 namespace Wibblr.Grufs.Cli
 {
+    /// <summary>
+    /// Usage:
+    ///   - grufs.exe command subcommand [options]
+    ///       where command is storage, backup, restore
+    /// 
+    ///  - storage subcommands:
+    ///      init, refresh, list
+    ///       
+    ///      - grufs.exe storage init --name mystorage --protocol sftp --host hostname --port port --user user --storage-password password --identity mykey.rsa --basedir ~/grufs-storage --encryption-password password
+    ///      - grufs.exe storage init --name mystorage ~/grufs-storage
+    ///   
+    ///      This will:
+    ///        - create the directory on the remote server
+    ///        - login to sftp if using public key authentication
+    ///        - prompt for ssh password if required
+    ///        - prompt for storage password if required
+    ///        - create the metadata for the default repository
+    ///        - show error if metadata already exists and password is incorrect
+    ///        - update local config with 'mystorage = sftp://user@hostname:port/grufs-storage'
+    ///     
+    ///       - grufs.exe storage list --verbose
+    /// 
+    ///       This will:
+    ///         - list all storages previously created
+    ///         - the verbose flag will include: creation date, list of backupsets.
+    ///     
+    ///       - grufs.exe storage refresh --protocol sftp ...
+    ///       
+    ///       This will:
+    ///       - download all information from storage
+    ///       
+    ///  - backup subcommands
+    ///    -- grufs.exe backup mybackup --storage mystorage  --directory 'c:\my documents' 
+    ///    -- grufs.exe backup mybackup
+    /// 
+    /// </summary>
+    /// 
+
+    class Arguments
+    {
+        //public string 
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -39,37 +81,42 @@ namespace Wibblr.Grufs.Cli
                 Console.WriteLine("Initialized repository");
             }
 
-            Console.WriteLine("1. Upload directory (non recursive)");
-            Console.WriteLine("2. List all directories");
-            var choice = Console.ReadLine();
-
-            switch (choice)
+            while (true)
             {
-                case "1":
-                    Console.WriteLine($"Enter local directory path");
-                    var localPath = Console.ReadLine() ?? throw new Exception();
+                Console.WriteLine("1. Upload directory (non recursive)");
+                Console.WriteLine("2. List all directories");
+                var choice = Console.ReadLine();
 
-                    if (!Directory.Exists(localPath))
-                    {
-                        Console.WriteLine($"No such directory: {localPath}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Enter remote directory path");
-                        var remotePath = Console.ReadLine() ?? throw new Exception();
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine($"Enter local directory path");
+                        var localPath = Console.ReadLine() ?? throw new Exception();
 
-                        Console.WriteLine($"Uploading {localPath} to {remotePath}");
-                        new MutableFilesystem(repo, new HmacKey(Convert.FromHexString("1111111111111111111111111111111111111111111111111111111111111111"))).UploadDirectoryNonRecursive(localPath, new DirectoryPath(remotePath));
-                    }
-                    break;
+                        if (!Directory.Exists(localPath))
+                        {
+                            Console.WriteLine($"No such directory: {localPath}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Enter remote directory path");
+                            var remotePath = Console.ReadLine() ?? throw new Exception();
 
-                case "2":
-                    new MutableFilesystem(repo, new HmacKey(Convert.FromHexString("1111111111111111111111111111111111111111111111111111111111111111"))).ListDirectory(new DirectoryPath("/"));
-                    break;
+                            Console.WriteLine($"Uploading {localPath} to {remotePath}");
+                            new MutableFilesystem(repo, new HmacKey(Convert.FromHexString("1111111111111111111111111111111111111111111111111111111111111111"))).UploadDirectoryNonRecursive(localPath, new DirectoryPath(remotePath));
+                        }
+                        continue;
 
-                default:
-                    break;
+                    case "2":
+                        new MutableFilesystem(repo, new HmacKey(Convert.FromHexString("1111111111111111111111111111111111111111111111111111111111111111"))).ListDirectory(new DirectoryPath("/"));
+                        continue;
 
+                    case "Q":
+                        break;
+
+                    default:
+                        continue;
+                }
             }
         }
     }
