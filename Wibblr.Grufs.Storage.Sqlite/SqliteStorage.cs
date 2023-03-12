89c61649336aa11c42ec4552ab17f595532e5412
aled <aled@wibblr.com>
2023-03-12 21:49:46 +0000

    Reuse sql commands in sqlite for better performance

--- Wibblr.Grufs.Storage.Sqlite/SqliteStorage.cs
+++ Wibblr.Grufs.Storage.Sqlite/SqliteStorage.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using System.Net;
+
+using Microsoft.Data.Sqlite;
 
 namespace Wibblr.Grufs.Storage.Sqlite
 {
@@ -7,6 +9,12 @@ namespace Wibblr.Grufs.Storage.Sqlite
         public string BaseDir { get; init; }
 
         private SqliteConnection _connection;
+        private SqliteCommand _insertCommand;
+        private SqliteCommand _upsertCommand;
+        private SqliteCommand _existsCommand;
+        private SqliteCommand _selectCommand;
+
+        private List<EncryptedChunk> _chunks = new List<EncryptedChunk>();
 
         public SqliteStorage(string path) 
         {
@@ -30,9 +38,38 @@ namespace Wibblr.Grufs.Storage.Sqlite
 
             using (var cmd = _connection.CreateCommand())
             {
-                cmd.CommandText = "PRAGMA page_size = 512; CREATE TABLE IF NOT EXISTS Chunk (Address BLOB NOT NULL PRIMARY KEY, Content BLOB NOT NULL) WITHOUT ROWID;";
+                cmd.CommandText = "PRAGMA page_size = 512;";
                 cmd.ExecuteNonQuery();
             }
+            using (var cmd = _connection.CreateCommand())
+            {
+                cmd.CommandText = "PRAGMA journal_mode=WAL";
+                var result = cmd.ExecuteScalar();
+                //Console.WriteLine(result);
+            }
+            using (var cmd = _connection.CreateCommand())
+            {
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS Chunk (Address BLOB NOT NULL PRIMARY KEY, Content BLOB NOT NULL) WITHOUT ROWID;";
+                cmd.ExecuteNonQuery();
+            }
+
+            _insertCommand = _connection.CreateCommand();
+            _insertCommand.CommandText = "INSERT OR IGNORE INTO Chunk (Address, Content) VALUES ($P1, $P2);";
+            _insertCommand.Parameters.Add(new SqliteParameter("$P1", SqliteType.Blob));
+            _insertCommand.Parameters.Add(new SqliteParameter("$P2", SqliteType.Blob));
+
+            _upsertCommand = _connection.CreateCommand();
+            _upsertCommand.CommandText = "INSERT INTO Chunk (Address, Content) VALUES ($P1, $P2) ON CONFLICT(Address) DO UPDATE SET Content = Excluded.Content;";
+            _upsertCommand.Parameters.Add(new SqliteParameter("$P1", SqliteType.Blob));
+            _upsertCommand.Parameters.Add(new SqliteParameter("$P2", SqliteType.Blob));
+
+            _existsCommand = _connection.CreateCommand();
+            _existsCommand.CommandText = "SELECT EXISTS(SELECT 1 FROM Chunk WHERE Address = $P1);";
+            _existsCommand.Parameters.Add(new SqliteParameter("$P1", SqliteType.Blob));
+
+            _selectCommand = _connection.CreateCommand();
+            _selectCommand.CommandText = "SELECT Content FROM Chunk WHERE Address = $P1;";
+            _selectCommand.Parameters.Add(new SqliteParameter("$P1", SqliteType.Blob));
         }
 
         public long Count()
@@ -46,12 +83,9 @@ namespace Wibblr.Grufs.Storage.Sqlite
 
         public bool Exists(Address address)
         {
-            using (var cmd = _connection.CreateCommand())
-            {
-                cmd.CommandText = "SELECT EXISTS(SELECT 1 FROM Chunk WHERE Address = $P1);";
-                cmd.Parameters.AddWithValue("$P1", address.ToSpan().ToArray());
-                return Convert.ToBoolean(cmd.ExecuteScalar());
-            }
+            var cmd = _existsCommand;
+            cmd.Parameters[0].Value = address.ToSpan().ToArray();
+            return Convert.ToBoolean(cmd.ExecuteScalar());
         }
 
         public IEnumerable<Address> ListAddresses()
@@ -63,26 +97,23 @@ namespace Wibblr.Grufs.Storage.Sqlite
         {
             try
             {
-                using (var cmd = _connection.CreateCommand())
+                var cmd = overwriteStrategy switch
                 {
-                    cmd.CommandText = overwriteStrategy switch
-                    {
-                        OverwriteStrategy.Deny => "INSERT OR IGNORE INTO Chunk (Address, Content) VALUES ($P1, $P2);",
-                        OverwriteStrategy.Allow => "INSERT INTO Chunk (Address, Content) VALUES ($P1, $P2) ON CONFLICT(Address) DO UPDATE SET Content = Excluded.Content;",
-                        _ => throw new Exception()
-                    };
-                    cmd.Parameters.AddWithValue("$P1", chunk.Address.ToSpan().ToArray());
-                    cmd.Parameters.AddWithValue("$P2", chunk.Content);
+                    OverwriteStrategy.Deny => _insertCommand,
+                    OverwriteStrategy.Allow => _upsertCommand,
+                    _ => throw new Exception()
+                };
+                cmd.Parameters[0].Value = chunk.Address.ToSpan().ToArray();
+                cmd.Parameters[1].Value = chunk.Content;
 
-                    int rowsInserted = cmd.ExecuteNonQuery();
+                int rowsInserted = cmd.ExecuteNonQuery();
 
-                    return rowsInserted switch
-                    {
-                        0 => PutStatus.OverwriteDenied,
-                        1 => PutStatus.Success,
-                        _ => PutStatus.Unknown
-                    };
-                }
+                return rowsInserted switch
+                {
+                    0 => PutStatus.OverwriteDenied,
+                    1 => PutStatus.Success,
+                    _ => PutStatus.Unknown
+                };
             }
             catch (Exception ex)
             {
@@ -92,18 +123,15 @@ namespace Wibblr.Grufs.Storage.Sqlite
 
         public bool TryGet(Address address, out EncryptedChunk chunk)
         {
-            using (var cmd = _connection.CreateCommand())
+            var cmd = _selectCommand;
+            cmd.Parameters[0].Value = address.ToSpan().ToArray();
+            using (var reader = cmd.ExecuteReader())
             {
-                cmd.CommandText = "SELECT Content FROM Chunk WHERE Address = $P1;";
-                cmd.Parameters.AddWithValue("$P1", address.ToSpan().ToArray());
-                using (var reader = cmd.ExecuteReader())
+                if (reader.Read())
                 {
-                    if (reader.Read())
-                    {
-                        var content = ((MemoryStream)reader.GetStream(reader.GetOrdinal("Content"))).ToArray();
-                        chunk = new EncryptedChunk(address, content);
-                        return true;
-                    }
+                    var content = ((MemoryStream)reader.GetStream(reader.GetOrdinal("Content"))).ToArray();
+                    chunk = new EncryptedChunk(address, content);
+                    return true;
                 }
             }
             chunk = default;
@@ -112,6 +140,8 @@ namespace Wibblr.Grufs.Storage.Sqlite
 
         public void Dispose()
         {
+            _insertCommand.Dispose();
+            _upsertCommand.Dispose();
             _connection.Close();
             _connection.Dispose();
             SqliteConnection.ClearAllPools(); // this is needed otherwise the database file will fail to delete
